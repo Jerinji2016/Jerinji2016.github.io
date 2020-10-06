64f4d858ea71fc2702b938f4e5338c3ea91b53de
Jerin P Jimmy <jerin.p@Jerin.local>
2020-10-06 20:32:02 +0530

    fix

--- scripts/languages.js
+++ scripts/languages.js
@@ -41,6 +41,7 @@ window.onload = function () {
         setStyle(item);
     });
 
+    if(isElementInViewport(document.getElementById('language-container')))
     addAnimations();
 
     document.getElementsByTagName('head')[0].appendChild(style);
@@ -57,5 +58,22 @@ function addAnimations() {
         div.style.width = `${item.val}%`;
     });
     animAdded = false;
-
 }
+
+function isElementInViewport(el) {
+    // special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
